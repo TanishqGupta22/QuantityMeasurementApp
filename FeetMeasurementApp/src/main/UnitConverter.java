893008510c2a2984a7e440e38a86a7598cc2227d
Tanishq Gupta <guptatanishq131@gmail.com>
2026-02-19 12:07:52 +0530

    [Tanishq Gupta] add . program for UC3- feature

---
+++ FeetMeasurementApp/src/main/UnitConverter.java
@@ -0,0 +1,12 @@
+package com.quantity;
+
+public class UnitConverter {
+
+    public static double feetToInches(double feet) {
+        return feet * 12;
+    }
+
+    public static double inchesToFeet(double inches) {
+        return inches / 12;
+    }
+}
