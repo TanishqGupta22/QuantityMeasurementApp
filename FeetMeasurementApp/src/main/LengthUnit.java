552fe206b29e7d970dd1105374289f91b397c4b6
Tanishq Gupta <guptatanishq131@gmail.com>
2026-02-20 13:56:43 +0530

    [Tanishq Gupta] add . program for UC4 feature

---
+++ FeetMeasurementApp/src/main/LengthUnit.java
@@ -0,0 +1,17 @@
+public enum LengthUnit {
+
+    FEET(12.0),
+    INCHES(1.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
+
+    private final double toInchesFactor;
+
+    LengthUnit(double toInchesFactor) {
+        this.toInchesFactor = toInchesFactor;
+    }
+
+    public double toInches(double value) {
+        return value * toInchesFactor;
+    }
+}
