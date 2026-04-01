38fe1a392e5ae28c9ca0638b0b7b783140636f16
Tanishq Gupta <guptatanishq131@gmail.com>
2026-04-01 16:46:17 +0530

    [Tanishq Gupta] refactor. final modification for authentication

--- quantityMeasurementApp/src/main/java/com/app/quantitymeasurement/repository/UserRepository.java
+++ quantityMeasurementApp/src/main/java/com/app/quantitymeasurement/repository/UserRepository.java
@@ -1,10 +1,9 @@
-package com.app.repository;
+package com.app.quantitymeasurement.repository;
 
-import com.app.model.User;
+import com.app.quantitymeasurement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByUsername(String username);
 }
--- quantityMeasurementApp/src/test/java/com/app/quantitymeasurement/QuantityMeasurementAppApplicationTests.java
+++ quantityMeasurementApp/src/test/java/com/app/quantitymeasurement/QuantityMeasurementAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.app.quantityMeasurementApp;
+package com.app.quantitymeasurement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
